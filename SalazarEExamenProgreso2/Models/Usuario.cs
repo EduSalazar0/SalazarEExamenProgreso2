e7cf6ca4183fafb342ba18b5163c5edac8d8fd20
EduSalazar0 <edutebansalazar95@gmail.com>
2024-12-02 07:23:15 -0500

    Creación de Modelo Usuario con sus atributos ademas de la creacion de Interfaces y una clase IUsuarioRepository.

---
+++ SalazarEExamenProgreso2/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalazarEExamenProgreso2.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El numero es necesario")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="El numero de telefono es necesario")]
+        public string Numero { get; set; }
+
+    }
+}
